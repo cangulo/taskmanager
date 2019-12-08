8de6f05dd977f0232b3ed4b72da9d80e10629721
Carlos Miguel Angulo Mascarell <carlos.angulo.mascarell@outlook.com>
2019-12-08 19:24:51 +0100

    Basic Log Decorator checking the result type and if the result is success

--- API/TaskManagerAPI/TaskManagerAPI.CQRS/HandlerDecorator/RequestHandlerLogDecorator.cs
+++ API/TaskManagerAPI/TaskManagerAPI.CQRS/HandlerDecorator/RequestHandlerLogDecorator.cs
@@ -1,24 +1,31 @@
 ﻿using MediatR;
 using Microsoft.Extensions.Logging;
+using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace TaskManagerAPI.CQRS.HandlerDecorator
 {
-    public class RequestHandlerLogDecorator<TRequest, TResponse> : IRequestHandler<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    public class RequestHandlerLogDecorator<TRequest, TResult> : IRequestHandler<TRequest, TResult> where TRequest : IRequest<TResult>
     {
-        private readonly IRequestHandler<TRequest, TResponse> _decorated;
-        private readonly ILogger<RequestHandlerLogDecorator<TRequest, TResponse>> _logger;
+        private readonly IRequestHandler<TRequest, TResult> _decorated;
+        private readonly ILogger<RequestHandlerLogDecorator<TRequest, TResult>> _logger;
 
-        public RequestHandlerLogDecorator(IRequestHandler<TRequest, TResponse> decorated, ILogger<RequestHandlerLogDecorator<TRequest, TResponse>> logger)
+        public RequestHandlerLogDecorator(IRequestHandler<TRequest, TResult> decorated, ILogger<RequestHandlerLogDecorator<TRequest, TResult>> logger)
         {
             _decorated = decorated;
             _logger = logger;
         }
 
-        public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken)
+        public async Task<TResult> Handle(TRequest request, CancellationToken cancellationToken)
         {
-            return _decorated.Handle(request, cancellationToken);
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            TResult response = await _decorated.Handle(request, cancellationToken);
+            stopwatch.Stop();
+            string handlerName = this._decorated.GetType().Name;
+            this._logger.LogInformation($"Handler;{handlerName};{stopwatch.ElapsedMilliseconds}");
+            return response;
         }
     }
 }
