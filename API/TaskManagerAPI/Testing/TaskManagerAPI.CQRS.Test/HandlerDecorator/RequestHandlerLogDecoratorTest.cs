af7c5b28a2161cbd483993b90e1c6af3fe8475dd
Carlos Miguel Angulo Mascarell <carlos.angulo.mascarell@outlook.com>
2020-02-02 16:44:14 +0100

    Progress in the UT

--- API/TaskManagerAPI/Testing/TaskManagerAPI.CQRS.Test/HandlerDecorator/RequestHandlerLogDecoratorTest.cs
+++ API/TaskManagerAPI/Testing/TaskManagerAPI.CQRS.Test/HandlerDecorator/RequestHandlerLogDecoratorTest.cs
@@ -3,7 +3,6 @@ using FluentResults;
 using MediatR;
 using Microsoft.Extensions.Logging;
 using Moq;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using TaskManagerAPI.CQRS.HandlerDecorator;
@@ -33,32 +32,18 @@ namespace TaskManagerAPI.CQRS.Test.HandlerDecorator
             RequestTestClass request = new RequestTestClass();
             ResultBase expectedResult = Results.Ok();
             _decorated.Setup(x => x.Handle(request, CancellationToken.None)).Returns(Task.FromResult(expectedResult));
-            _logger.Setup(x => x.LogInformation(It.IsAny<string>()));
 
             // Act
             ResultBase result = await _handlerDecorator.Handle(request, CancellationToken.None);
 
             // Assert
             result.Should().Be(expectedResult);
-            _logger.Verify(x => x.LogInformation(It.IsAny<string>()), Times.Once);
         }
 
-        //[Fact]
-        //public async Task Handle_Error_ThrowException()
-        //{
-        //    // Arrange
-        //    RequestTestClass request = new RequestTestClass();
-        //    ResultBase expectedResult = Results.Fail(new Error());
-        //    _decorated.Setup(x => x.Handle(request, CancellationToken.None)).Returns(Task.FromResult(expectedResult));
-        //    _logger.Setup(x => x.LogError(It.IsAny<string>()));
-
-        //    // Act
-        //    ResultBase result = await _handlerDecorator.Handle(request, CancellationToken.None);
-
-        //    // Assert
-        //    result.Should().Be(expectedResult);
-        //    _logger.Verify(x => x.LogError(It.IsAny<string>()), Times.Exactly(2));
-        //}
-
+        [Fact]
+        public void Handle_Error_ThrowException()
+        {
+            // TODO
+        }
     }
 }
