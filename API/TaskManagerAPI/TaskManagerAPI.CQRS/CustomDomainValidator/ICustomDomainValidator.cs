4194cbd521a80eb29fdc9be9b88b24d494220bba
Carlos Miguel Angulo Mascarell <carlos.angulo.mascarell@outlook.com>
2020-02-16 21:55:43 +0100

    [AddComment]

--- API/TaskManagerAPI/TaskManagerAPI.CQRS/CustomDomainValidator/ICustomDomainValidator.cs
+++ API/TaskManagerAPI/TaskManagerAPI.CQRS/CustomDomainValidator/ICustomDomainValidator.cs
@@ -2,6 +2,7 @@
 
 namespace TaskManagerAPI.CQRS.DomainValidatorModel
 {
+    // Not used yet
     public interface ICustomDomainValidator<TValidator,TRequest>
     {
         ValidationResult Validate(TRequest request);
