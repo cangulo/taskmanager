93e01d5f2da81106d5090814d8cbbf751bf38eb4
Carlos Miguel Angulo Mascarell <carlos.angulo.mascarell@outlook.com>
2020-02-21 00:36:21 +0100

    Improved syntax for creating ValidatorDecorators

--- API/TaskManagerAPI/TaskManagerAPI/Extensions/AutofacModules/CQRSModule.cs
+++ API/TaskManagerAPI/TaskManagerAPI/Extensions/AutofacModules/CQRSModule.cs
@@ -15,34 +15,30 @@ namespace TaskManagerAPI.Extensions.AutofacModules
     {
         protected override void Load(ContainerBuilder containerBuilder)
         {
-            List<Type> classes = typeof(DeleteTaskCommandValidator).Assembly.GetTypes().ToList();
-            List<Type> validators = classes.Where(x => x.Name.EndsWith("Validator")).ToList();
-            foreach (var validator in validators)
-            {
-                Console.WriteLine(validator.Namespace);
+            var validators = typeof(DeleteTaskCommandValidator)
+                .Assembly
+                .GetTypes()
+                .Where(x => x.Name.EndsWith("Validator"))
+                .ToArray();
 
-                Type requestType = validator.BaseType.GenericTypeArguments[0];
-                Type responseType = requestType.GetInterfaces()[0].GenericTypeArguments[0];
+            containerBuilder
+                .RegisterTypes(validators)
+                .As((validator) =>
+                    {
+                        Type requestType = validator.BaseType.GenericTypeArguments[0];
+                        return typeof(ICustomDomainValidator<>).MakeGenericType(new Type[] { requestType });
+                    }
+                );
 
-
-                Type interfaceCustomValidatorType = typeof(ICustomDomainValidator<>);
-                Type validatorInterfaceTyped = interfaceCustomValidatorType.MakeGenericType(new Type[] { requestType });
-
-                containerBuilder.RegisterType(validator).As(validatorInterfaceTyped);
-
-
-                Type validatorDecoratorGeneric = typeof(RequestHandlerValidatorDecorator<,>);
-                Type[] decoratorArgs = new Type[] { requestType, responseType };
-                Type validatorDecoratorSpecific = validatorDecoratorGeneric.MakeGenericType(decoratorArgs);
-
-
-                Type requestHandlerGeneric = typeof(IRequestHandler<,>);
-                Type requestHandlerSpecific = requestHandlerGeneric.MakeGenericType(decoratorArgs);
-
-                containerBuilder
-                    .RegisterDecorator(validatorDecoratorSpecific, requestHandlerSpecific);
-
-            }
+            containerBuilder
+                .RegisterGenericDecorator(
+                    typeof(RequestHandlerValidatorDecorator<,>),
+                    typeof(IRequestHandler<,>),
+                    (context) =>
+                    {
+                        string requestTypeName = context.ServiceType.GetGenericArguments()[0].Name;
+                        return validators.Any(x => x.Name == requestTypeName + "Validator");
+                    });
 
             containerBuilder
                 .RegisterGenericDecorator(typeof(RequestHandlerLogDecorator<,>), typeof(IRequestHandler<,>));
